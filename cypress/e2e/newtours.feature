3ced977513d80b868e5b4dc0c167f631828dea3b
zaenal fahmi <zaenalfahmi34@gmail.com>
2024-01-18 00:05:59 +0700

    Update_TestData Using Parameters as a String

--- cypress/e2e/newtours.feature
+++ cypress/e2e/newtours.feature
@@ -1,6 +1,7 @@
 Feature: Newtours Validation
 
 Scenario: Home Page
-  Given precondition
-  When action
-  Then testable outcome
+  Given open newtours application
+  When provide valid 'mercury' and 'mercury'
+  Then click on submit button
+  And verify title should be 'Login: Mercury Tours'
--- cypress/e2e/newtours/newtours.js
+++ cypress/e2e/newtours/newtours.js
@@ -1,13 +1,19 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps"
 
-Given("precondition", ()=>{
+Given("open newtours application", ()=>{
     cy.visit("https://demo.guru99.com/test/newtours")
 })
 
-When("action", ()=>{
+When("provide valid {string} and {string}", (userName, password)=>{
     cy.title().should('eq', 'Welcome: Mercury Tours')
+    cy.get('[name = userName]').type(userName)
+    cy.get('[name= password]').type(password)
 })
 
-Then("testable outcome", ()=>{
-    cy.title().should('eq', 'Welcome: Mercury Tours')
+Then("click on submit button", ()=>{
+    cy.get('[name = submit]').click()
+})
+
+And("verify title should be {string}", (title)=>{
+    cy.title().should('eq', title)
 })
